fabae4a844875d1db5775e8f87f5b2de6742bbb6
arijit adhikary <arijitadhikary@gmail.com>
2019-10-22 22:58:55 -0400

    AA - Updates

--- resources/sql/insert-data.sql
+++ resources/sql/insert-data.sql
@@ -1,4 +1,4 @@
-insert  into `user_detail`(`name`,`user_name`,`email`,`password`) values ('User1','username1', 'user1@gmail.com', '123');
+insert  into `user_detail`(`name`,`user_name`,`email`,`password`) values ('User1','username1','user1@gmail.com', '123');
 insert  into `user_detail`(`name`,`user_name`,`email`,`password`) values ('User2','username2','user2@gmail.com', '1234');
 insert  into `user_detail`(`name`,`user_name`,`email`,`password`) values ('User3','username3','user3@gmail.com', '12345');
 insert  into `user_detail`(`name`,`user_name`,`email`,`password`) values ('User4','username4','user4@gmail.com', '123456');
