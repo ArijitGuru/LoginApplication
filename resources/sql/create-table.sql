0176fe9c50c1f28828ba6b24fc8eb661255cc203
arijit adhikary <arijitadhikary@gmail.com>
2019-11-01 08:56:56 -0400

    AA - updates

--- resources/sql/create-table.sql
+++ resources/sql/create-table.sql
@@ -8,10 +8,10 @@ CREATE TABLE `user_detail` (
   PRIMARY KEY (`id`)
 );
 
-CREATE TABLE user_roles (
-  user_role_id int(11) NOT NULL AUTO_INCREMENT,
-  username varchar(45) NOT NULL,
-  role varchar(45) NOT NULL,
-  PRIMARY KEY (user_role_id),
-  UNIQUE KEY uni_username_role (role,username),
-  CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES user_detail (username));
+CREATE TABLE `user_roles` (
+  `user_role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `role` varchar(45) NOT NULL,
+  PRIMARY KEY (`user_role_id`),
+  UNIQUE KEY uni_username_role (`role`,`username`),
+  CONSTRAINT fk_username FOREIGN KEY (`username`) REFERENCES user_detail (`username`));
