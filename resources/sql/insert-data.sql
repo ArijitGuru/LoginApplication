4894fd361962865822ace4938bac0795e5c09f87
arijit adhikary <arijitadhikary@gmail.com>
2019-11-04 22:58:20 -0500

    AA - updates

--- resources/sql/insert-data.sql
+++ resources/sql/insert-data.sql
@@ -1,10 +1,9 @@
-insert  into `user_detail`(`name`,`username`,`email`,`password`,`enabled`) values ('User1','username1','user1@gmail.com', '123', true);
+insert  into `user_detail`(`name`,`username`,`email`,`password`,`enabled`) values ('User1','Admin','user1@gmail.com', '123', true);
 insert  into `user_detail`(`name`,`username`,`email`,`password`,`enabled`) values ('User2','username2','user2@gmail.com', '1234', true);
-insert  into `user_detail`(`name`,`username`,`email`,`password`,`enabled`) values ('User3','username3','user3@gmail.com', '12345', true);
-insert  into `user_detail`(`name`,`username`,`email`,`password`,`enabled`) values ('User4','username4','user4@gmail.com', '123456', true);
 
 
 
-INSERT INTO `authorities` (`username`, `role`) VALUES ('username1', 'ROLE_ADMIN');
+
+INSERT INTO `authorities` (`username`, `role`) VALUES ('Admin', 'ROLE_ADMIN');
 INSERT INTO `authorities` (`username`, `role`) VALUES ('username2', 'ROLE_USER');
 
